4119b52983b584afc57717e47bccf5361b73ebba
Robert <robbtish@gmail.com>
2022-06-24 13:41:33 +0500

    Only proto file is good

--- sendImg.py
+++ sendImg.py
@@ -1,11 +1,3 @@
-"""syntax = "proto3";
-message Image {
-    int32 id = 1;
-    bytes data = 2;
-    int32 width = 3;
-    int32 height = 4;
-    //int32 channels = 5;
-  }"""
 import sendImg_pb2
 image = sendImg_pb2.Image()
 image.id = 1234
